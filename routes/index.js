156eb99cf6beb9ab71f67701516c8d704ac5b01f
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-11 00:55:19 +0900

    implement lctp with cron

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const axios = require("axios");
+const cron = require("node-cron");
+const router = express.Router();
 
+
+const query = '\n    query questionOfToday {\n  activeDailyCodingChallengeQuestion {\n    date\n    userStatus\n    link\n    question {\n      acRate\n      difficulty\n      freqBar\n      frontendQuestionId: questionFrontendId\n      isFavor\n      paidOnly: isPaidOnly\n      status\n      title\n      titleSlug\n      hasVideoSolution\n      hasSolution\n      topicTags {\n        name\n        id\n        slug\n      }\n    }\n  }\n}\n    '
 let todayProblemLink = null
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  if(!todayProblemLink) updateTodayProblemLink();
+/*-----------------init region---------------------*/
+updateTodayProblemLink()
+/*-------------------------------------------------*/
 
+router.get('/', function(req, res) {
   res.send(todayProblemLink)
 });
 
 function updateTodayProblemLink() {
-  todayProblemLink = 'test'
+  axios.post('https://leetcode.com/graphql/', {query: query}).then(response => {
+    todayProblemLink = `https://leetcode.com${response.data["data"]["activeDailyCodingChallengeQuestion"]["link"]}`
+  })
 }
+
+cron.schedule('15 0 9 * * *', updateTodayProblemLink, {
+  timezone: "Asia/Seoul"
+});
 
 module.exports = router;
