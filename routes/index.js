fe31292e8973cbe852c5b4eb2ac7ea53aa65fdac
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-12 00:57:22 +0900

    Install expose-gc and apply it

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require("axios");
 const cron = require("node-cron");
 const router = express.Router();
+require('expose-gc');
 
 
 const query = '\n    query questionOfToday {\n  activeDailyCodingChallengeQuestion {\n    date\n    userStatus\n    link\n    question {\n      acRate\n      difficulty\n      freqBar\n      frontendQuestionId: questionFrontendId\n      isFavor\n      paidOnly: isPaidOnly\n      status\n      title\n      titleSlug\n      hasVideoSolution\n      hasSolution\n      topicTags {\n        name\n        id\n        slug\n      }\n    }\n  }\n}\n    '
@@ -21,20 +22,12 @@ function updateTodayProblemLink() {
   })
 }
 
-function forceGC(){
-  if (global.gc) {
-    global.gc();
-  } else {
-    console.warn('No GC hook! Start your program as `node --expose-gc file.js`.');
-  }
-}
-
-cron.schedule('0 10/10 * * * *', () => {
+cron.schedule('0 */5 * * * *', () => {
   const used = process.memoryUsage();
   for (let key in used) {
     console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
   }
-  forceGC();
+  global.gc();
 });
 
 cron.schedule('0/5 0 9 * * *', updateTodayProblemLink, {
