10951dca71e302bfec552ca9e302dc56e49adca4
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2023-01-12 00:10:31 +0900

    Add memory usage log

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,13 @@ function updateTodayProblemLink() {
   })
 }
 
+cron.schedule('*/30 * * * * *', () => {
+  const used = process.memoryUsage();
+  for (let key in used) {
+    console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
+  }
+});
+
 cron.schedule('0/5 0 9 * * *', updateTodayProblemLink, {
   timezone: "Asia/Seoul"
 });
